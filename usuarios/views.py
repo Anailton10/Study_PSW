9ba286df0451be07ec992e2c74ad382f36cce478
Anailton Silva <francisco.anailton17@gmail.com>
2024-01-15 21:51:53 -0300

    validações para view cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            return redirect(reverse(cadastro))
+            # return redirect('/usuarios/cadastro')
+
+        user = User.objects.all()
+
+        if user.exists():
+            return redirect(reverse(cadastro))
+
+        try:
+            User.objects.create_user(
+                username=username,
+                password=senha
+            )
+            return redirect(reverse(login))
+        except:
+            return redirect(reverse(cadastro))
+
+    return HttpResponse('teste')
+
+
+def login(request):
+    return HttpResponse('Você está no login')
